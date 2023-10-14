de12f7c3915020df94f5b791591e365345556243
Callum Brocklehurst <60849655+CallumBrocklehurst@users.noreply.github.com>
2023-10-14 11:08:57 +0100

    take a screenshot
    * take a screenshot
    * platform differences

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
+import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
+
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -12,35 +16,39 @@ import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 
-const PlaceholderImage = require('./assets/images/background-image.png')
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [showAppOptions, setShowAppOptions] = useState(false);
+  const imageRef = useRef();
+
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  if (status === null) {
+    requestPermission();
+  }
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true, 
+      allowsEditing: true,      
       quality: 1,
     });
+
     if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri)
-      setShowAppOptions(true)
+      setSelectedImage(result.assets[0].uri);      
+      setShowAppOptions(true);
     } else {
-      alert('You didnt select any image')
+      alert('You did not select any image.');
     }
-  }
+  };
 
   const onReset = () => {
     setShowAppOptions(false);
   };
 
-  const onSaveImageAsync = async () => {
-    // we will implement this later
-  };
-  
   const onAddSticker = () => {
     setIsModalVisible(true);
   };
@@ -49,24 +57,61 @@ export default function App() {
     setIsModalVisible(false);
   };
 
+    const onSaveImageAsync = async () => {
+      if (Platform.OS !== 'web') {
+        try {
+          const localUri = await captureRef(imageRef, {
+            height: 440,
+            quality: 1,
+          });
+    
+          await MediaLibrary.saveToLibraryAsync(localUri);
+          if (localUri) {
+            alert("Saved!");
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        try {
+          const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+            quality: 0.95,
+            width: 320,
+            height: 440,
+          });
+    
+          let link = document.createElement('a');
+          link.download = 'sticker-smash.jpeg';
+          link.href = dataUrl;
+          link.click();
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    };
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
         </View>
+      </View>      
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
-        <View style={styles.optionsRow}>
-          <IconButton icon="refresh" label="Reset" onPress={onReset} />
-          <CircleButton onPress={onAddSticker} />
-          <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
         </View>
-      </View>
-      ) : ( 
+      ) : (
         <View style={styles.footerContainer}>
-          <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
-          <Button label='Use this photo' onPress={() => setShowAppOptions(true)}/>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button
+            label="Use this photo" onPress={() => setShowAppOptions(true)}
+          />
         </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
@@ -77,18 +122,19 @@ export default function App() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
   },
-  imageContainer : {
-    flex: 1, 
-    paddingTop: 58,
+  imageContainer: {
+    flex:1, 
+    paddingTop: 58
   },
-  footerContainer : {
-    flex: 1 / 3, 
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: 'center',
   },
   optionsContainer: {
@@ -98,5 +144,6 @@ const styles = StyleSheet.create({
   optionsRow: {
     alignItems: 'center',
     flexDirection: 'row',
+    justifyContent: 'center',
   },
 });
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,4 +1,5 @@
 import { View, Image } from 'react-native';
+
 import { PanGestureHandler, TapGestureHandler} from "react-native-gesture-handler";
 
 import Animated, {
@@ -6,17 +7,22 @@ import Animated, {
   useSharedValue,
   useAnimatedGestureHandler,
   withSpring,
-
 } from 'react-native-reanimated';
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
 const AnimatedView = Animated.createAnimatedComponent(View);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
-
-  const scaleImage = useSharedValue(imageSize);
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
+  const scaleImage = useSharedValue(imageSize);
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
 
   const onDoubleTap = useAnimatedGestureHandler({
     onActive: () => {
@@ -24,13 +30,6 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
         scaleImage.value = scaleImage.value * 2;
       }
     },
-  });
-
-  const imageStyle = useAnimatedStyle(() => {
-    return {
-      width: withSpring(scaleImage.value),
-      height: withSpring(scaleImage.value),
-    };
   });
 
   const onDrag = useAnimatedGestureHandler({
@@ -59,7 +58,7 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
 
   return (
     <PanGestureHandler onGestureEvent={onDrag}>
-      <AnimatedView style={{ top: -350 }}>
+      <AnimatedView style={[containerStyle, { top: -350 }]}>
         <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
           <AnimatedImage
             source={stickerSource}
